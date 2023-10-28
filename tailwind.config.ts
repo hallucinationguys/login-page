3ca65282534d0277403a8f460bcc3d2df507046b
datnguyennnx <official.nguyendat@gmail.com>
2023-10-28 20:12:23 +0700

    chore: Fixing rule format and add library for layout

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,4 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   mode: 'jit',
@@ -28,6 +28,6 @@ const config: Config = {
     },
   },
   plugins: [],
-}
+};
 
-export default config
+export default config;
