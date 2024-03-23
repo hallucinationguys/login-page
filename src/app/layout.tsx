a32449bbf5bdb4b9a0ae43fdbc146568464f602d
datnguyennnx <official.nguyendat@gmail.com>
2024-03-24 00:29:09 +0700

    chore: Add favicon for head element

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,6 +20,15 @@ export default function RootLayout({
           content="Login service from Hallucination Guys"
           name="description"
         />
+        <link rel="icon" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="icon" sizes="32x32" href="/favicon-32x32.png" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link rel="icon" sizes="192x192" href="/android-chrome-192x192.png" />
+        <link rel="icon" sizes="512x512" href="/android-chrome-512x512.png" />
       </head>
       <body className={inter.className}>
         <AuthContextProvider>
